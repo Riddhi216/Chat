0a84ef4b27513eb9e0ced7abb458494de31ba173
Riddhi216 <155568577+Riddhi216@users.noreply.github.com>
2025-03-13 09:08:56 +0530

    Initial commit - uploading existing project

---
+++ server/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+
+from .models import Server
+
+class ServerListViewSet(viewsets.ViewSet):
+    queryset = Server.objects.all()
+
+    def list(self, request):
+        category = request.query_param.get("category")
+         
+        if category :
+           self.queryset.filter(category=category)
